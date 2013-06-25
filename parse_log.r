7673587da32f26d6ab1af339d6c25caec7603d4f
unknown <skozyr@.(none)>
2013-06-25 11:51:18 +0300

    Coinvert timestamp

--- parse_log.r
+++ parse_log.r
@@ -1,3 +1,6 @@
 Sys.setlocale(category="LC_TIME", "C")
 
 log2 <- read.table("access.extend.log-20130527", quote="\"")
+
+#convert timestamp
+log2$V2 <- as.POSIXct(strptime(substr(paste(log2$V4, log2$V5), 2, 27), format="%d/%b/%Y:%H:%M:%S %z"))
