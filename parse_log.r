fbb9b46062f59b9b9906057159558dd40a67a7f1
unknown <skozyr@.(none)>
2013-06-25 11:51:44 +0300

    Remove unused fields. Fixed spaces in url

--- parse_log.r
+++ parse_log.r
@@ -4,3 +4,7 @@ log2 <- read.table("access.extend.log-20130527", quote="\"")
 
 #convert timestamp
 log2$V2 <- as.POSIXct(strptime(substr(paste(log2$V4, log2$V5), 2, 27), format="%d/%b/%Y:%H:%M:%S %z"))
+log2$V5 <- NULL
+log2$V6 <- as.character(log2$V6)
+log2[log2$V6=="-", "V6"] <- "- - -" #Must be two spaces in this field
+log2$V6 <- as.factor(log2$V6)
